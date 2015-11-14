242396a7f00794eced2321208b736b7e1d3f51be
Devansh Jain <devansh@cutozcom>
2015-11-14 12:31:01 +0000

    Added File Upload Module

---
+++ learningphp/file_upload.php
@@ -0,0 +1,45 @@
+<?php
+    $target_dir = "Upload/";
+    $target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
+    $upload_ok = 1;
+    
+    if(isset($_POST["submit"]))
+    {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check!==false)
+        {
+            echo "File is an image" .$check["mime"].".";
+            $upload_ok = 1;
+        }
+        
+        else
+        {
+            echo "File is not a Image";
+            $upload_ok = 0;    
+        }
+        
+        if (file_exists($target_file)) {
+          echo "Sorry, file already exists.";
+            $uploadOk = 0;
+            
+        }
+            
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        }
+        else
+        {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+            {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            }
+            else
+            {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+        
+        
+    }
+    
