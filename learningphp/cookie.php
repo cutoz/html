0f86c5c57c619fa4ccc838be7d4165bdee77b937
Devansh Jain <devansh@cutozcom>
2015-11-14 13:05:12 +0000

    Added cookie.php

---
+++ learningphp/cookie.php
@@ -0,0 +1,14 @@
+<?php
+    $cookie_name = "com.srmadt.cooks";
+    $cookie_value = "com.srmadt.cooks_value";
+    if(!isset($_COOKIE[$cookie_name]))
+    {
+        echo "Cookie ".$cookie_name." was not set";
+        setcookie($cookie_name,$cookie_value,time() + 300,"/");
+    }
+    else
+    {
+        echo "Cookie ".$cookie_name." is Set!";
+    }
+    ?>
+    
